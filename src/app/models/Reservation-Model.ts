f53426f243660bb6d019f4b6d8d658d8c14d691f
DavideGenito <davidegenito2008@gmail.com>
2026-06-01 15:22:16 +0200

    Sistemato user

--- src/app/models/Reservation-Model.ts
+++ src/app/models/Reservation-Model.ts
@@ -1,7 +1,7 @@
 import { User } from "./User";
 
-export class ReservationModel{
-    user: User = new User
+export class ReservationModel {
+    user: User = new User('', '', '', 0, true, '')
     date: Date = new Date()
     time: String = ''
     slotId: Number = 0
