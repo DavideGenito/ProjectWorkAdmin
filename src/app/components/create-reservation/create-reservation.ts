3cc78ff4e5027e4ed0bbfc961bf518951a934532
DavideGenito <davidegenito2008@gmail.com>
2026-06-01 10:24:27 +0200

    Prima commit struttura

---
+++ src/app/components/create-reservation/create-reservation.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-reservation',
+  imports: [],
+  templateUrl: './create-reservation.html',
+  styleUrl: './create-reservation.css',
+})
+export class CreateReservation {}
---
+++ src/app/components/view-reservation/view-reservation.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-reservation',
+  imports: [],
+  templateUrl: './view-reservation.html',
+  styleUrl: './view-reservation.css',
+})
+export class ViewReservation {}
